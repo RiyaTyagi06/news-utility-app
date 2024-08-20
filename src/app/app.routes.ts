185a4c5d6414118d5f1782d0e4de202efd80a716
Riya Tyagi <riyatyagi892@gmail.com>
2024-08-20 23:38:43 +0530

    created components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { NewsListComponent } from './news-list/news-list.component';
+import { NewsDetailComponent } from './news-detail/news-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+export const routes: Routes = [
+  { path: 'detail', component: NewsDetailComponent },
+  { path: 'list', component: NewsListComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
